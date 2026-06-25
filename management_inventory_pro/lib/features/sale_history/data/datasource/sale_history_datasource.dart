7d6ad8872bf2a5f6263994dec7f9b5aa4d763655
Mahmoud <mahmoudsayeedr80@gmail.com>
2026-06-25 18:35:35 +0300

    sale_history_datasource.dart

---
+++ management_inventory_pro/lib/features/sale_history/data/datasource/sale_history_datasource.dart
@@ -0,0 +1,71 @@
+import 'package:management_inventory_pro/core/database/database_constants.dart';
+import 'package:management_inventory_pro/core/networking/api_error_model.dart';
+import 'package:management_inventory_pro/core/networking/api_result.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/sale_item_model.dart';
+import '../models/sale_model.dart';
+
+class SaleHistoryDatasource {
+  final Database _database;
+
+  const SaleHistoryDatasource(this._database);
+
+  Future<ApiResult> getSales() async {
+   try{
+     final rows = await _database.rawQuery('''
+SELECT
+    s.${DatabaseConstants.idColumn} AS ${DatabaseConstants.saleIdColumn},
+    s.${DatabaseConstants.totalItemColumn},
+    s.${DatabaseConstants.totalQuantityColumn},
+    s.${DatabaseConstants.totalAmountColumn},
+    s.${DatabaseConstants.noteColumn},
+    s.${DatabaseConstants.createdAtColumn},
+    s.${DatabaseConstants.updatedAtColumn},
+    s.${DatabaseConstants.paymentMethodColumn},
+    s.${DatabaseConstants.cashierNameColumn},
+    s.${DatabaseConstants.statusColumn},
+
+    si.id AS ${DatabaseConstants.saleItemId},
+    si.${DatabaseConstants.productIdColumn},
+    si.${DatabaseConstants.quantityColumn},
+    si.${DatabaseConstants.sellingPriceColumn},
+    si.${DatabaseConstants.totalColumn},
+
+    p.${DatabaseConstants.nameColumn},
+    p.${DatabaseConstants.skuColumn},
+    p.${DatabaseConstants.barcodeColumn},
+    p.${DatabaseConstants.imageUrlColumn},
+    p.${DatabaseConstants.sellingPriceColumn} AS ${DatabaseConstants.productPrice}
+
+FROM ${DatabaseConstants.saleTable} s
+
+INNER JOIN ${DatabaseConstants.saleItemTable} si
+    ON si.${DatabaseConstants.saleIdColumn} = s.${DatabaseConstants.idColumn}
+
+INNER JOIN ${DatabaseConstants.productTable} p
+    ON p.${DatabaseConstants.idColumn} = si.${DatabaseConstants.productIdColumn}
+
+ORDER BY s.${DatabaseConstants.createdAtColumn} DESC
+''');
+
+     final Map<String, SaleModel> salesMap = {};
+
+     for (final row in rows) {
+       final saleId = row['sale_id'] as String;
+
+       final item = SaleItemModel.fromMap(row);
+
+       if (!salesMap.containsKey(saleId)) {
+         salesMap[saleId] = SaleModel.fromMap( map: row,saleId: saleId,);
+
+       }
+       salesMap[saleId]!.items.add(item);
+     }
+
+     return ApiResult.success(salesMap.values.toList());
+   }catch(e){
+     return ApiResult.failure(ApiErrorModel(message: e.toString()));
+   }
+
+  }
+}
